d58cdede3e0918981ae61ac4c210c8ddd7e96d89
gutovesco <guto.vesco@hotmail.com>
2020-05-20 14:59:31 -0300

    added different movies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,19 +19,27 @@ export class AppComponent {
   },
     {
       trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-      cover: 'assets/images/guardios-da-galaxia.jpg',
-      name: 'Guardiões da Galaxia 2',
+      cover: 'assets/images/the-rain.jpg',
+      name: 'The Rain',
       relevance: 99,
       age: 16,
       parts: 2,
-      categories: ['Épico', 'Fantasia', 'Viagem no espaço']
+      categories: ['Drama', 'Suspense', 'Ação']
     },{
       trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-      cover: 'assets/images/guardios-da-galaxia.jpg',
-      name: 'Guardiões da Galaxia 3',
+      cover: 'assets/images/o-menino-que-descobriu-o-vento.jpg',
+      name: 'O menino que descobriu o vento',
       relevance: 100,
       age: 18,
       parts: 1,
-      categories: ['Épico', 'Fantasia', 'Viagem no espaço']
+      categories: ['Drama', 'Suspense', 'Ação']
+    },{
+      trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover: 'assets/images/better-call-saul.jpg',
+      name: 'Better Call Saul',
+      relevance: 100,
+      age: 18,
+      parts: 1,
+      categories: ['Comédia', 'Suspense', 'Ação']
     },]
 }
